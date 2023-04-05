af324367c2c23c9012b7a49812ffbe6accb99180
Emmanuel Baah Duodu <duoduemmanuelbaah@gmail.com>
2023-04-05 16:42:45 +0000

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+function Footer(){
+    return(
+        <div className="footer">
+	<div className="container">
+		<div className="row">
+			<p className="text-center">
+				&copy; 2022 Premier Pizza All Rights Reserved
+			</p>
+		</div>
+	</div>
+</div>
+    );
+}
+
+export default Footer;
---
+++ src/PizzaTypes.js
@@ -0,0 +1,69 @@
+function PizzaTypes(){
+    return(
+        <div className="container pizza types">
+	<div className="row">
+		<h3 className="text-center">Pizza Types</h3>
+
+		<div className="col-md-3">
+			<div className="thumbnail">
+				<img src="pizza images/pizza-6.jpg" alt="vegetable" />
+				<div className="caption">
+					<h3>Vegetable Pizza</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+					</p>
+				</div>
+			</div>
+		</div>
+		
+		<div className="col-md-3">
+			<div className="thumbnail">
+				<img src="pizza images/pizza-7.jpg" alt="pepperoni" />
+				<div className="caption">
+					<h3>Pepperoni Pizza</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+					</p>
+				</div>
+			</div>
+		</div>
+		<div className="col-md-3">
+			<div className="thumbnail">
+				<img src="pizza images/pizza-8.jpg" alt="egg" />
+				<div className="caption">
+					<h3>Egg Pizza</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+					</p>
+				</div>
+			</div>
+		</div>
+		
+		<div className="col-md-3">
+			<div className="thumbnail">
+				<img src="pizza images/pizza-9.jpg" alt="cheese" />
+				<div className="caption">
+					<h3>Cheese Pizza</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+					</p>
+				</div>
+			</div>
+		</div>
+		
+
+	</div>
+	 
+</div>
+    )
+}
+
+export default PizzaTypes;
